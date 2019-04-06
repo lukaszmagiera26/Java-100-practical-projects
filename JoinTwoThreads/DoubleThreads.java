25c6063d79ae6c1bc4d9520b57abf0ff65c5acb0
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-06 11:11:22 +0200

    U

---
+++ JoinTwoThreads/DoubleThreads.java
@@ -0,0 +1,39 @@
+
+package JoinTwoThreads;
+
+public class DoubleThreads {
+
+	public static void main(String args[]) throws InterruptedException {
+		Thread FirstThread = new Thread() {
+			public void run() {
+				try {
+					System.out.println(Thread.currentThread().getName() + " JustStarted");
+					Thread.sleep(1000);
+					System.out.println(Thread.currentThread().getName() + " JustCompleted");
+				} catch (InterruptedException ex) {
+
+				}
+			}
+		};
+
+		Thread SecondThread = new Thread() {
+			public void run() {
+				try {
+					System.out.println(Thread.currentThread().getName() + " JustStarted");
+					Thread.sleep(1000);
+					System.out.println(Thread.currentThread().getName() + " JustCompleted");
+				} catch (InterruptedException ex) {
+
+				}
+			}
+		};
+
+		FirstThread.start();
+		FirstThread.join();
+
+		SecondThread.start();
+		SecondThread.join();
+
+		System.out.println(Thread.currentThread().getName() + " Just Completed");
+	}
+}
