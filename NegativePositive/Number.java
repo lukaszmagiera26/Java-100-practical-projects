9d9a027cf0ebb09cc2a34188a37c1f2054b0cfe4
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-07 12:41:58 +0200

    Up

---
+++ NegativePositive/Number.java
@@ -0,0 +1,19 @@
+package NegativePositive;
+
+import java.util.*;
+
+public class Number {
+
+	public static void main(String args[]) {
+		int x;
+		System.out.println("Please insert any number: ");
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+
+		if (x < 0) {
+			System.out.println("This is a negative number");
+		} else
+			System.out.println("This is a positive number");
+	}
+
+}
