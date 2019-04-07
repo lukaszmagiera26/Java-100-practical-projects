fc7d6e57ba1d4e0b4eb9e190810e6370b905e75f
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-07 12:38:27 +0200

    Up

---
+++ MethodOverloading/Overload.java
@@ -0,0 +1,25 @@
+package MethodOverloading;
+
+import java.io.*;
+
+public class Overload {
+
+	String add(String a, String b) {
+		return (a + b);
+	}
+
+}
+
+class StringOverLoad {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String x, y;
+		System.out.println("Please Enter two values: ");
+		x = br.readLine();
+		y = br.readLine();
+
+		Overload ol = new Overload();
+		System.out.print("Result of your two values are: " + ol.add(x, y));
+	}
+}
