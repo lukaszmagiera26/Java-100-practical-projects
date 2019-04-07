3d409b2fa0f50d82a78a9f04be27e939dc8144e4
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-07 12:37:12 +0200

    Up

---
+++ MergeArray/Array.java
@@ -0,0 +1,17 @@
+package MergeArray;
+
+import java.util.*;
+
+public class Array {
+
+	public static void main(String args[]) {
+		String x[] = { "H", "E", "L", "L", "O" };
+		String y[] = { "W", "O", "R", "L", "D" };
+
+		List ls = new ArrayList(Arrays.asList(x));
+		ls.addAll(Arrays.asList(y));
+
+		Object[] z = ls.toArray();
+		System.out.println(Arrays.toString(z));
+	}
+}
