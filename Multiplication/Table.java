77fedf1862099b4ba4f0d03458e32200f35c8ba4
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-07 12:41:37 +0200

    Up

---
+++ Multiplication/Table.java
@@ -0,0 +1,17 @@
+package Multiplication;
+
+import java.util.Scanner;
+
+public class Table {
+
+	public static void main(String args[]) {
+		int x, y;
+		System.out.println("Please insert a number to print Multiplication Table");
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		System.out.println("Result of Multiplication Table of " + x + " is: ");
+		for (y = 1; y <= 20; y++)
+			System.out.println(x + "*" + y + "=" + (x * y));
+	}
+
+}
