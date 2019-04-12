3ec162429b68c9c52a4a063f26764640d32491a9
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-13 00:07:18 +0200

    UP

---
+++ Print/Writer.java
@@ -0,0 +1,18 @@
+package Print;
+
+import java.io.PrintWriter;
+
+public class Writer {
+
+	public static void main(String[] args) {
+		
+		PrintWriter pw = new PrintWriter(System.out, true);
+		String st = "project";
+		pw.println("This is " + st);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
