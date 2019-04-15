f74386ef88267612c5f7d5a3c8746b0a11058557
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-15 17:31:16 +0200

    UP

---
+++ PrintMatrix/MatrixPrinter.java
@@ -0,0 +1,24 @@
+package PrintMatrix;
+
+import java.io.*;
+
+public class MatrixPrinter {
+
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num1[][] = new int[3][3];
+		int x, y;
+		System.out.println("Please, enter 9 Integer value: ");
+		for (x = 0; x < 3; x++) {
+			for (y = 0; y < 3; y++) {
+				num1[x][y] = Integer.parseInt(br.readLine());
+			}
+		}
+		for (x = 0; x < num1.length; x++) {
+			for (y = 0; y < 3; y++) {
+				System.out.println(" " + num1[x][y]);
+			}
+			System.out.println();
+		}
+	}
+}
